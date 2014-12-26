784cc92282a7fc135e185ca463f486869dde8275
lillian815 <woyaofeixiangyan@163.com>
2014-12-26 14:12:39 +0800

    orm

---
+++ Watch_orm/src/net/tsz/afinal/bitmap/core/BitmapDisplayConfig.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2012-2013, Michael Yang 杨福海 (www.yangfuhai.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.tsz.afinal.bitmap.core;
+
+import android.graphics.Bitmap;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+
+public class BitmapDisplayConfig {
+	
+	public static final int GREY = 1;
+	
+	private int bitmapWidth;
+	private int bitmapHeight;
+	
+	private Animation animation;
+	
+	private int animationType;
+	private Bitmap loadingBitmap;
+	private Bitmap loadfailBitmap;
+	private ScaleType scaleType = ImageView.ScaleType.FIT_XY;
+	private int type = 0;
+	
+
+	public int getBitmapWidth() {
+		return bitmapWidth;
+	}
+
+	public void setBitmapWidth(int bitmapWidth) {
+		this.bitmapWidth = bitmapWidth;
+	}
+
+	public int getBitmapHeight() {
+		return bitmapHeight;
+	}
+
+	public void setBitmapHeight(int bitmapHeight) {
+		this.bitmapHeight = bitmapHeight;
+	}
+
+	public Animation getAnimation() {
+		return animation;
+	}
+
+	public void setAnimation(Animation animation) {
+		this.animation = animation;
+	}
+
+	public int getAnimationType() {
+		return animationType;
+	}
+
+	public void setAnimationType(int animationType) {
+		this.animationType = animationType;
+	}
+
+	public Bitmap getLoadingBitmap() {
+		return loadingBitmap;
+	}
+
+	public void setLoadingBitmap(Bitmap loadingBitmap) {
+		this.loadingBitmap = loadingBitmap;
+	}
+
+	public Bitmap getLoadfailBitmap() {
+		return loadfailBitmap;
+	}
+
+	public void setLoadfailBitmap(Bitmap loadfailBitmap) {
+		this.loadfailBitmap = loadfailBitmap;
+	}
+
+	
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+
+	public ScaleType getScaleType() {
+		return scaleType;
+	}
+
+	public void setScaleType(ScaleType scaleType) {
+		this.scaleType = scaleType;
+	}
+
+
+	public class AnimationType{
+		public static final int userDefined = 0;
+		public static final int fadeIn = 1;
+	}
+
+}
